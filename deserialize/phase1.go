4a3c6d07dea6e4f27209b7b3054cb20652d18357
Kobi Gurkan <kobigurk@gmail.com>
2023-06-22 20:19:55 +0300

    adds gnark import

---
+++ deserialize/phase1.go
@@ -0,0 +1,9 @@
+package deserializer
+
+import (
+	"github.com/consensys/gnark/backend/groth16/bn254/mpcsetup"
+)
+
+func readPtauToSrs() (mpcsetup.Phase1, error) {
+	return mpcsetup.Phase1{}, nil
+}
